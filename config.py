a399ff86acb9dfa868ccd59497fefa9c8fbc88d6
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-09-09 22:37:58 +0300

    Add photo uploading

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maxa:maxa12345@localhost/pitches'
     SQLALCHELMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     pass
