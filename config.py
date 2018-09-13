4a10beadcd0c13736c7a305aff9f63f4352bcf54
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-09-13 16:07:20 +0300

    redeployment

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maxa:maxa12345@localhost/pitches'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maxa:maxa12345@localhost/pitches'
     # SQLALCHELMY_TRACK_MODIFICATIONS = False
     '''
      the destination to store images
@@ -20,16 +20,12 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-
-# class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maxa:maxa12345@localhost/pitches_test'
-
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maxa:maxa12345@localhost/pitches'
+    
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
-# 'test':TestConfig
 }
--- manage.py
+++ manage.py
@@ -2,26 +2,22 @@ from app import create_app,db
 from flask import Flask
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User,Role,Pitch
+from app.models import User,Role,Pitch,Comment
 
 
 # Creating app instance
-#app=create_app('development')
+app=create_app('development')
 app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Role = Role,Pitch=Pitch)
+    return dict(app = app,db = db,User = User,Role = Role,Pitch=Pitch,Comment=Comment)
+
+
 if __name__ == '__main__':
     manager.run()
